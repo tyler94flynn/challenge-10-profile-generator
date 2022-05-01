bfebe6584065b3f36a58b4778e11d65006fb6ba5
tyler94flynn <tyler94flynn@gmail.com>
2022-05-01 12:37:14 -0400

    boilerplate generatehtml & css

---
+++ src/generateHMTL.js
@@ -0,0 +1,21 @@
+const generateHTML = (data) => {
+    return `<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Team Details</title>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+            <link rel="stylesheet" href="./dist/style.css">
+        </head>
+
+        <body>
+            <div class="jumbotron jumbotron-fluid" style='background-color: red;'>
+                <div class="container">
+                    <h1 class="display-4 text-center">Team Details</h1>
+                </div>
+            </div>
+        </body>
+        </html>`;
+};
