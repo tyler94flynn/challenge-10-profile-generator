4b4a66398e8e274574878adeedc517c1083bdac3
tyler94flynn <tyler94flynn@gmail.com>
2022-05-01 19:02:23 -0400

    added functions to collect prompt data

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 
+//feeds user input to html template
+const generateHTML = require("./src/generateHTML");
+
 //classes
 const Manager = require("./lib/Manager");
 
@@ -9,6 +12,147 @@ const Engineer = require("./lib/Engineer");
 
 const Intern = require("./lib/Intern");
 
-//questions
-const team = [];
+//questions 
+const employees = [];
+
+//Manager object gathers manager details & gives the wizard a prompt to add other employee details
+const askManager = [{
+    type: "input",
+    name: "name",
+    message: "Please input the manager's name.",
+},
+{
+    type: "input",
+    name: "id",
+    message: "Please input the manager's id.",
+},
+{
+    type: "input",
+    name: "email",
+    message: "Please input the manager's email.",
+},
+{
+    type: "input",
+    name: "officeNumber",
+    message: "Please input the manager's office number.",
+},
+{
+    type: "list",
+    name: "employeeList",
+    message: "Select the role of the team member you'd like to add next. If you are done, select \"I'm done.\"",
+    choices: ["Engineer", "Intern", "I'm done."],
+},
+];
+
+//Engineer method does the same as manager object but as a function
+const askEngineer = () => {
+    inquirer
+        .prompt([{
+                type: "input",
+                name: "name",
+                message: "Please input the engineer's name.",
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "Please input the engineer's ID.",
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Please input the engineer's email.",
+            },
+            {
+                type: "input",
+                name: "GitHub",
+                message: "Please input the engineer's GitHub username.",
+            },
+            {
+                type: "list",
+                name: "employeeList",
+                message: "Select the role of the team member you'd like to add next. If you are done, select \"I'm done.\"",
+                choices: ["Engineer", "Intern", "I'm done."],
+            },
+        ])
+        .then((data) => {
+            teamMembers.push(
+                new Engineer(data.name, data.id, data.email, data.GitHub)
+            );
+            if (data.employeeList === "Engineer") {
+                askEngineer();
+            } else if (data.employeeList === "Intern") {
+                askIntern();
+            } else {
+                let data = generateHTML(teamMembers);
+                fs.writeFileSync("./dist/employees.html", data, "utf-8");
+            }
+        });
+};
+
+//Intern method functions same as engineer method
+const askIntern = () => {
+    inquirer
+        .prompt([{
+                type: "input",
+                name: "name",
+                message: "What id your intern's name?",
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is your intern's id?",
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is your intern's email?",
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "What is your intern's school?",
+            },
+            {
+                type: "list",
+                name: "employeeList",
+                message: "Which type of team member would you like to add?",
+                choices: ["Engineer", "Intern", "I'm done."],
+            },
+        ]).then((data) => {
+            teamMembers.push(
+                new Intern(data.name, data.id, data.email, data.school)
+            );
+            if (data.employeeList === "Engineer") {
+                askEngineer();
+            } else if (data.employeeList === "Intern") {
+                askIntern();
+            } else {
+                let data = generateHTML(teamMembers);
+                fs.writeFileSync("employees.html", data, "utf-8");
+            }
+        });
+};
+
+//wizard prompt
+const init = () => {
+    inquirer.prompt(askManager).then((data) => {
+        employees.push(
+            new Manager(data.name, data.id, data.email, data.officeNumber)
+        );
+        if (data.employeeList === "Engineer") {
+            askEngineer();
+        } else if (data.employeeList === "Intern") {
+            askIntern();
+        } else {
+            //end function here and generateHTML
+            var data = generateHTML(employees);
+            fs.writeFileSync("./dist/employees.html", data, "utf-8");
+        }
+    });
+};
+
+//runs wizard
+init();
+
+
 
