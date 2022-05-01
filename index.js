c38c15a3e346f1d19e6dda130cb56b22ff5806f8
tyler94flynn <tyler94flynn@gmail.com>
2022-05-01 00:24:55 -0400

    filed out classes

---
+++ index.js
@@ -0,0 +1,10 @@
+//packages
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+//classes
+const Manager = require("./lib/Manager");
+
+const Engineer = require("./lib/Engineer");
+
+const Intern = require("./lib/Intern");
